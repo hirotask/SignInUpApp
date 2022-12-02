458c5cc83208481f7dfc43cb645e548dd631ab62
hirotask <hirotaskhashi@gmail.com>
2022-12-02 14:53:55 +0900

    [move]: componentsパッケージの中を移動

---
+++ app/src/main/java/me/hirotask/loginformcompose/ui/TodoPage.kt
@@ -0,0 +1,15 @@
+package me.hirotask.loginformcompose.ui.components.pages
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun TodoPage() {
+
+}
+
+@Preview(showSystemUi = true)
+@Composable
+fun TodoPagePreview() {
+
+}
