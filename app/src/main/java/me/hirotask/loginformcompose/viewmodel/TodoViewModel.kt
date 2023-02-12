d197dba83562be521a31d4a2ff10eee4bf3062cb
hirotask <hirotaskhashi@gmail.com>
2023-02-12 11:40:39 +0900

    [fix]: LiveData->StateFlow

--- app/src/main/java/me/hirotask/loginformcompose/viewmodel/TodoViewModel.kt
+++ app/src/main/java/me/hirotask/loginformcompose/viewmodel/TodoViewModel.kt
@@ -4,6 +4,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import me.hirotask.loginformcompose.model.firebase.FirebaseAuthRepository
 import me.hirotask.loginformcompose.model.firebase.FirestoreRepository
@@ -12,8 +14,9 @@ import me.hirotask.loginformcompose.model.util.getCompleted
 import java.util.*
 
 class TodoViewModel: ViewModel() {
-    private val _list = MutableLiveData<List<Todo>>()
-    val list: LiveData<List<Todo>> = _list
+
+    private val _list = MutableStateFlow(listOf<Todo>())
+    val list: StateFlow<List<Todo>> = _list
 
     fun addTodo(content: String, priority: String, limit: Date, memo: String): Boolean {
         val todo = Todo.create(content, priority, limit, memo, false)
@@ -29,7 +32,7 @@ class TodoViewModel: ViewModel() {
     }
 
     fun completeTodo(todo: Todo) {
-        _list.value = _list.value?.getCompleted(todo)
+        _list.value = _list.value.getCompleted(todo)
     }
 
 
