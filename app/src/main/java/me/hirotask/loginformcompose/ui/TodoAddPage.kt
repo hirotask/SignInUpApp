7b78681bd11fe2f5148a9fe70be8c88338529637
hirotask <hirotaskhashi@gmail.com>
2023-02-12 11:46:42 +0900

    [fix]: 月が1カ月ずれていたのを修正

--- app/src/main/java/me/hirotask/loginformcompose/ui/TodoAddPage.kt
+++ app/src/main/java/me/hirotask/loginformcompose/ui/TodoAddPage.kt
@@ -38,7 +38,7 @@ fun TodoAddPage(
     val context = LocalContext.current
     val calendar = Calendar.getInstance()
     val year = calendar.get(Calendar.YEAR)
-    val month = calendar.get(Calendar.MONTH)
+    val month = calendar.get(Calendar.MONTH) + 1
     val day = calendar.get(Calendar.DAY_OF_MONTH)
 
     val scaffoldState = rememberScaffoldState()
